4c44183366cace1e5de7e42cdb3141dabc7acb4f
hanbyul <7777ccc13@gmail.com>
2026-07-28 22:00:26 +0900

    Revert "채팅 내역 삭제·이름변경 UI와 돈워리 로고 추가, API 키를 .env로 분리했어요"
    This reverts commit a02f48411671a774b956929cc772723ea9955632.

--- webapp/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")"
-export PYTHONPATH="$(pwd)/pylibs:$PYTHONPATH"
-export HF_HOME="$(pwd)/hf_cache"
-export TRANSFORMERS_CACHE="$(pwd)/hf_cache"
-exec python3 -u app.py
